6bf12ff627fb6eab53026e67bebb24be979ce1d9
macabernardini-star <macabernardini@gmail.com>
2025-11-25 16:03:30 -0300

    Add files via upload

---
+++ tpfinal/js/biografia.js
@@ -0,0 +1,5 @@
+document.getElementById("menu-btn").addEventListener("click", () => {
+  const menu = document.getElementById("menu");
+  menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
+});
+
